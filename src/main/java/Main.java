13cdcc0888db494c6ae0c240cd26048370edfcd7
Giovanni Lenguito <giovanni16.gl@gmail.com>
2016-10-20 23:22:11 +0100

    UI: - Implements input field for file path - Implements grid layout - Updates scene size - Implements validation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
 import creation.BuildClass;
 import creation.ClassWriter;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import objects.*;
 import org.jsoup.Jsoup;
@@ -26,6 +31,9 @@ public class Main extends Application
 {
     static Stage stage;
     static StackPane pane;
+    static TextField inputField;
+    static GridPane grid;
+    static  Label startLabel;
 
     private static Map<String, String[]> elementMap = new HashMap<>();
     private static List<Relation> relations = new ArrayList<>();
@@ -43,15 +51,27 @@ public class Main extends Application
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //Sets
         pane = new StackPane();
         stage = primaryStage;
+        inputField = new TextField();
 
-        primaryStage.setTitle("SVG to Java");
+        //setup grid
+        grid = new GridPane();
+        grid.setAlignment((Pos.CENTER));
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25,25,25,25));
 
-        //Create button
+        Scene scene = new Scene(grid, 340, 200);
+
+        //create controls
+        Text title = new Text("Welcome to SVG to Java");
         Button startBtn = new Button();
         startBtn.setText("Start Conversion");
 
+
+        //set action on button click
         startBtn.setOnAction(event -> {
             try {
                 beginConversion();
@@ -60,76 +80,91 @@ public class Main extends Application
             }
         });
 
-        startBtn.setTranslateX(-190);
-        startBtn.setTranslateY(-80);
-        pane.getChildren().add(startBtn);
+        startLabel = new Label("Begin by entering the path of the .svg file");
+
+        //add controls to grid
+        grid.add(title, 0, 0, 2, 1);
+        grid.add(inputField, 0, 2);
+        grid.add(startBtn, 1, 2);
+        grid.add(startLabel, 0, 1, 2, 1);
+
+        //for debugging
+        //grid.setGridLinesVisible(true);
+
+        primaryStage.setTitle("SVG to Java");
+        //pane.getChildren().add(startBtn);
 
         //create window and show
-        primaryStage.setScene(new Scene(pane, 500, 200));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
     public static void beginConversion() throws Exception{
         //Create starting label
-        Label startLabel = new Label("Starting...");
-        pane.getChildren().add(startLabel);
-        pane.requestLayout();
+        if(inputField.getText().isEmpty()){
+            startLabel.setText("Please enter path...");
+            pane.requestLayout();
+        }else {
+            startLabel.setText("Starting");
+            pane.requestLayout();
 
-        final File file = new File( "src/main/resources/DiagramToCodeSVG.svg" );
-        final Document doc = Jsoup.parse( file, "UTF-8", "http://example.com/" );
-        //Elements textTag = doc.getElementsByTag( "text" );
 
-        for( Element element : doc.getAllElements() )
-        {
-            Elements locationElement = element.select( "[transform*=translate]" );
-            if( !locationElement.isEmpty() )
-            {
-                String locationAttribute = locationElement.select( "g" ).attr( "transform" );
-                Elements textElements = locationElement.select( "text" );
+            final File file = new File(inputField.getText());//"src/main/resources/DiagramToCodeSVG.svg" );
+            try {
+                final Document doc = Jsoup.parse(file, "UTF-8", "http://example.com/");
+                //Elements textTag = doc.getElementsByTag( "text" );
 
-                Elements titleElements = locationElement.select( "title" );
-                String enumTitle = CheckInput.checkTitle( titleElements.text() );
-                if( enumTitle != null )
-                {
-                    String theText = textElements.get( 0 ).text();
-                    String[] stringArray = { theText, enumTitle };
-                    elementMap.put( locationAttribute, stringArray );
+                for (Element element : doc.getAllElements()) {
+                    Elements locationElement = element.select("[transform*=translate]");
+                    if (!locationElement.isEmpty()) {
+                        String locationAttribute = locationElement.select("g").attr("transform");
+                        Elements textElements = locationElement.select("text");
+
+                        Elements titleElements = locationElement.select("title");
+                        String enumTitle = CheckInput.checkTitle(titleElements.text());
+                        if (enumTitle != null) {
+                            String theText = textElements.get(0).text();
+                            String[] stringArray = {theText, enumTitle};
+                            elementMap.put(locationAttribute, stringArray);
+                        }
+                    }
                 }
+
+                for (Map.Entry<String, String[]> m : elementMap.entrySet()) {
+                    String location = m.getKey().replaceAll("[^\\d.]", " ").trim().replace(" ", ",").split(",")[0];
+                    String value = m.getValue()[0];
+                    String type = m.getValue()[1];
+
+                    relations.add(new Relation(location, value, type));
+                }
+
+                List<ClazzToBuild> clazzToBuilds = new ArrayList<>();
+
+                for (ArrayList<ClassMember> classMembers : SortClasses.arrangeMethodAndVariables(relations)) {
+                    for (ClassMember tempObject : classMembers) {
+                        CheckInput.checkMember(tempObject);
+                    }
+
+                    clazzToBuilds.add(new ClazzToBuild(classMembers.get(0).getClassName(), ParseVariable.parseVariable(classMembers), ParseMethod.parseMethod(classMembers), ParseConstructor.parseConstructor(classMembers)));
+                }
+
+                // Always goes after the above loop.
+                //OutputClasses.outputClasses( clazzToBuilds );
+
+                for (ClazzToBuild clazzToBuild : clazzToBuilds) {
+                    //            BuildClass.buildClass(clazzToBuilds.get(2));
+                    //            break;
+                    String clazz = BuildClass.buildClass(clazzToBuild);
+                    ClassWriter.classWriter(clazz, clazzToBuild.getClassName());
+                    //break;
+                }
+                startLabel.setText("Complete");
+                pane.requestLayout();
+            }catch(Exception ex){
+                startLabel.setText("Invalid file path");
+                pane.requestLayout();
+                System.out.println(ex);
             }
-        }
-
-        for( Map.Entry<String, String[]> m : elementMap.entrySet() )
-        {
-            String location = m.getKey().replaceAll( "[^\\d.]", " " ).trim().replace( " ", "," ).split( "," )[0];
-            String value = m.getValue()[0];
-            String type = m.getValue()[1];
-
-            relations.add( new Relation( location, value, type ) );
-        }
-
-        List<ClazzToBuild> clazzToBuilds = new ArrayList<>();
-
-        for( ArrayList<ClassMember> classMembers : SortClasses.arrangeMethodAndVariables( relations ) )
-        {
-            for( ClassMember tempObject : classMembers )
-            {
-                CheckInput.checkMember( tempObject );
-            }
-
-            clazzToBuilds.add( new ClazzToBuild( classMembers.get( 0 ).getClassName(),
-                    ParseVariable.parseVariable( classMembers ), ParseMethod.parseMethod( classMembers ), ParseConstructor.parseConstructor( classMembers )) );
-        }
-
-        // Always goes after the above loop.
-        //OutputClasses.outputClasses( clazzToBuilds );
-
-        for( ClazzToBuild clazzToBuild : clazzToBuilds )
-        {
-            //            BuildClass.buildClass(clazzToBuilds.get(2));
-            //            break;
-            String clazz = BuildClass.buildClass(clazzToBuild);
-            ClassWriter.classWriter(clazz,clazzToBuild.getClassName());
-            //break;
         }
     }
 }
