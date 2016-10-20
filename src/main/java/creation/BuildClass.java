0aaef05ca62b3abb9652baa691bdf8c3c4ce4a6c
Tom <Cumminsc9@hotmail.co.uk>
2016-10-20 17:43:27 +0100

    Adhoc - Update

---
+++ src/main/java/creation/BuildClass.java
@@ -0,0 +1,13 @@
+package creation;
+
+import objects.ClazzToBuild;
+
+/**
+ * Created by Tom on 20/10/2016.
+ */
+public class BuildClass
+{
+    public void buildClass( final ClazzToBuild clazzToBuild )
+    {
+    }
+}
