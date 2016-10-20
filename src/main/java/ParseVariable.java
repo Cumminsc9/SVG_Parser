b4b800cc04193f20ec1d31b7cb54164433c9ed44
anilrahman <r008611b@student.staffs.ac.uk>
2016-10-20 17:36:47 +0100

    Adhoc - Update

--- src/main/java/ParseVariable.java
+++ src/main/java/ParseVariable.java
@@ -1,8 +1,11 @@
+import enums.AccessType;
 import enums.Title;
 import objects.Attribute;
+import objects.Method;
 
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
 /**
@@ -14,6 +17,72 @@ public class ParseVariable
 
     public static List<Attribute> parseVariable(final ArrayList<ClassMembers> method )
     {
+        List<Attribute> variableList = new ArrayList<>();
+        //LinkedHashMap<String, String> newHashMap = null;
+        String accessType = null;
+        String variableName = null;
+        String returnType = null;
+
+
+        for( ClassMembers classMembers : method )
+        {
+            if( !classMembers.getClassType().equals( Title.CONSTRUCTOR.getType() ) && !classMembers.getClassType().equals( Title.METHOD.getType() ) )
+            {
+                if( classMembers.getClassType().equals( Title.VARIABLE.getType() ) )
+                {
+                    final String classValue = classMembers.getClassValue();
+
+                    variableName = parseVariableName( classValue );
+                    accessType = parseAccessType( classValue );
+                    returnType = parseReturnType(classValue);
+
+
+                    if( accessType != null && variableName != null && returnType != null )
+                    {
+                        variableList.add( new Attribute( accessType, returnType, variableName ) );
+                    }
+                }
+            }
+        }
         return variableList;
     }
+
+
+    private static String parseVariableName( final String stringMethod )
+    {
+        return stringMethod.split( "[^\\w\\s]" )[1].trim();
+    }
+
+    private static String parseReturnType( final String stringMethod )
+    {
+        if(stringMethod.contains("["))//contains characters
+        {
+            return stringMethod.split(" ")[3];
+        }
+        return stringMethod.split( "[^\\w\\s]" )[2].trim();
+    }
+
+    private static String parseAccessType( final String stringMethod )
+    {
+        // Regex string = "^[^(]\w*"
+        final String accessType = stringMethod.substring( 0, 1 );
+
+        if( accessType.equals( AccessType.PUBLIC.getAccessType() ) )
+        {
+            return "public";
+        }
+        else if( accessType.equals( AccessType.PROTECTED.getAccessType() ) )
+        {
+            return "protected";
+        }
+        else if( accessType.equals( AccessType.PRIVATE.getAccessType() ) )
+        {
+            return "private";
+        }
+        else
+        {
+            return "public";
+        }
+    }
+
 }
--- src/main/java/objects/Attribute.java
+++ src/main/java/objects/Attribute.java
@@ -8,4 +8,39 @@ public class Attribute
     private String attributeName;
     private String attributeType;
     private String attributeAccessType;
+
+    public Attribute(String attributeAccessType, String attributeType, String attributeName) {
+        this.attributeAccessType = attributeAccessType;
+        this.attributeType = attributeType;
+        this.attributeName = attributeName;
+    }
+
+    public String getAttributeName() {
+        return attributeName;
+    }
+
+    public void setAttributeName(String attributeName) {
+        this.attributeName = attributeName;
+    }
+
+    public String getAttributeType() {
+        return attributeType;
+    }
+
+    public void setAttributeType(String attributeType) {
+        this.attributeType = attributeType;
+    }
+
+    public String getAttributeAccessType() {
+        return attributeAccessType;
+    }
+
+    public void setAttributeAccessType(String attributeAccessType) {
+        this.attributeAccessType = attributeAccessType;
+    }
+
+    @Override
+    public String toString() {
+        return attributeAccessType + "\t" + attributeType + "\t" + attributeName;
+    }
 }
