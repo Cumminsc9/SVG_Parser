e6fb5dbae821697ea631a4646e374e6e73b44d85
Giovanni Lenguito <giovanni16.gl@gmail.com>
2016-10-20 22:28:21 +0100

    - Implements start of Java FX UI - Implements button to begin conversion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,14 @@
 import creation.BuildClass;
 import creation.ClassWriter;
 import helpers.OutputClasses;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import objects.*;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
@@ -11,15 +19,15 @@ import parsers.ParseVariable;
 
 import java.io.File;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 /**
  * Created by c015406c on 18/10/2016.
  */
-public class Main
+
+public class Main extends Application
 {
+
     private static Map<String, String[]> elementMap = new HashMap<>();
     private static List<Relation> relations = new ArrayList<>();
 //
@@ -28,11 +36,37 @@ public class Main
 //        System.out.println(matcher.find());
 //    }
 
-
-    public static void main( String[] args ) throws Exception
-    {
+    public static void main( String[] args ) throws Exception {
         //new Main();
 
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("SVG to Java");
+
+        //Create button
+        Button btn = new Button();
+        btn.setText("Start Conversion");
+
+        btn.setOnAction(event -> {
+            try {
+                BeginConversion();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        //create window and show
+        primaryStage.setScene(new Scene(root, 300, 200));
+        primaryStage.show();
+    }
+
+    public static void BeginConversion() throws Exception{
         final File file = new File( "src/main/resources/DiagramToCodeSVG.svg" );
         final Document doc = Jsoup.parse( file, "UTF-8", "http://example.com/" );
         //Elements textTag = doc.getElementsByTag( "text" );
@@ -83,8 +117,8 @@ public class Main
 
         for( ClazzToBuild clazzToBuild : clazzToBuilds )
         {
-//            BuildClass.buildClass(clazzToBuilds.get(2));
-//            break;
+            //            BuildClass.buildClass(clazzToBuilds.get(2));
+            //            break;
             String clazz = BuildClass.buildClass(clazzToBuild);
             ClassWriter.classWriter(clazz,clazzToBuild.getClassName());
             //break;
